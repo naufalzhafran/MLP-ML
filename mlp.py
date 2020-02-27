250a19d8b60d67e0473acd859c43aa714c5a36f1
naufalzhafran <latifnaufal7@gmail.com>
2020-02-27 11:34:25 +0700

    add some comment

--- mlp.py
+++ mlp.py
@@ -32,24 +32,28 @@ class mlp:
     def __init__(self,input_nodes=0,learning_rate=.1,n_batch=1):
         self.number_of_nodes = []
         if input_nodes > 0:
-            self.number_of_nodes.append(input_nodes)
+            self.number_of_nodes.append(input_nodes) # add input layer
         self.weights = []
         self.biases = []
-        self.functions = []
         self.learning_rate = learning_rate
-        self.n_batch = n_batch
+        self.n_batch = n_batch # Belum dipakai
 
     def add_layer(self, number_of_nodes: int,function="sigmoid"):
+        '''
+        Adding layer(Hidden or Output)
+        '''
         self.number_of_nodes.append(number_of_nodes)
 
         if len(self.number_of_nodes) > 1:
             self.weights.append(np.random.randn(self.number_of_nodes[-1], self.number_of_nodes[-2]) * np.sqrt(2 / (self.number_of_nodes[-1] + self.number_of_nodes[-2])))
-            self.functions.append(function)
 
         if len(self.number_of_nodes) > 1:
             self.biases.append(np.random.uniform(0, 0, size=(number_of_nodes, 1)))
 
     def feed_forward(self,input_data):
+        '''
+        Feed foward data, outputs = array of output for every layer, output[-1] = output of last layer/output layer
+        '''
         outputs = [np.matrix(input_data).T]
 
         for i in range(len(self.number_of_nodes) - 1):
@@ -58,6 +62,9 @@ class mlp:
         return outputs
 
     def train(self, inp, targets,epoch=5,error_threshold=5):
+        '''
+        Train the mlp
+        '''
         for n in range(0,epoch):
             sum_error = 0
             for x in range(len(inp)):
@@ -67,15 +74,15 @@ class mlp:
 
                 # Calculate each layer error
                 errors = [np.subtract(target, outputs[-1])]
-                sum_error += np.sum(np.power(np.subtract(target, outputs[-1]),2))
+                sum_error += np.sum(np.power(np.subtract(target, outputs[-1]),2)) # Calculate square error for every data
                 for i in range(len(self.weights) - 1):
                     errors.insert(0, np.dot(self.weights[-1-i].T, errors[0]))
 
                 for i in range(len(self.weights)):
                     # Calculate gradient and weight correction
-                    gradient = np.multiply(errors[-1-i], np.multiply(outputs[-1-i] , (1-outputs[-1-i] )))
+                    gradient = np.multiply(errors[-1-i], np.multiply(outputs[-1-i] , (1-outputs[-1-i] ))) # Calculate gradient for sigmoid activation function
                     gradient *= self.learning_rate
-                    self.biases[-1-i] += gradient
+                    self.biases[-1-i] += gradient # Update biases
                     delta_w  = np.dot(gradient, outputs[-2-i].T)
                     self.weights[-1-i] += delta_w
             print("error :",sum_error/2,"Progress : ",n,"/",epoch)
