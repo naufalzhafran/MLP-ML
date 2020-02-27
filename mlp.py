485356856103e0f04413d3f51c7ec7f4e1d5b073
Hilmi Naufal <41366525+hilminaufal86@users.noreply.github.com>
2020-02-27 18:50:16 +0800

    add update weight per batch

--- mlp.py
+++ mlp.py
@@ -34,9 +34,12 @@ class mlp:
         if input_nodes > 0:
             self.number_of_nodes.append(input_nodes) # add input layer
         self.weights = []
+        self.delta_weights = []
         self.biases = []
+        self.delta_biases = []
         self.learning_rate = learning_rate
         self.n_batch = n_batch # Belum dipakai
+        self.number_of_layer = 0
 
     def add_layer(self, number_of_nodes: int,function="sigmoid"):
         '''
@@ -46,9 +49,13 @@ class mlp:
 
         if len(self.number_of_nodes) > 1:
             self.weights.append(np.random.randn(self.number_of_nodes[-1], self.number_of_nodes[-2]) )
+            self.delta_weights.append(np.zeros((number_of_nodes,self.number_of_nodes[self.number_of_layer])).tolist())
 
         if len(self.number_of_nodes) > 1:
+            self.delta_biases.append(np.zeros((number_of_nodes)).tolist())
             self.biases.append(np.random.uniform(0, 0, size=(number_of_nodes, 1)))
+
+        self.number_of_layer += 1
 
     def feed_forward(self,input_data):
         '''
@@ -65,9 +72,11 @@ class mlp:
         '''
         Train the mlp
         '''
+        counter = 0
         for n in range(0,epoch):
             sum_error = 0
             for x in range(len(inp)):
+                counter += 1
                 target = np.matrix(targets[x]).T
                 # Calculate output with given input
                 outputs = self.feed_forward(inp[x])
@@ -78,6 +87,7 @@ class mlp:
                 for i in range(len(self.weights) - 1):
                     errors.insert(0, np.dot(self.weights[-1-i].T, errors[0]))
 
+                """
                 for i in range(len(self.weights)):
                     # Calculate gradient and weight correction
                     gradient = np.multiply(errors[-1-i], np.multiply(outputs[-1-i] , (1-outputs[-1-i] ))) # Calculate gradient for sigmoid activation function
@@ -85,16 +95,70 @@ class mlp:
                     self.biases[-1-i] += gradient # Update biases
                     delta_w  = np.dot(gradient, outputs[-2-i].T)
                     self.weights[-1-i] += delta_w
+                """
+                
+                self.update_delta_weights(outputs,target)
+
+                if counter>=self.n_batch or x==len(inp)-1:
+                    self.update_weights()
+                    counter = 0
+
             print("error :",sum_error/2,"Progress : ",n,"/",epoch)
+    
+    def delta(self,layer,t_node,s_node,output,target):
+        """
+        Implementation of delta rule for weight a to b => w(s_node)(t_node)
+
+        dEtotal/dw[s_node][t_node] = (dEtotal/dout[t_node]) x (dout[t_node]/dnet[t_node]) * (dnet[t_node]/dw[s_node][t_node])
+        dEtotal/dw[s_node][t_node] = delta[t_node] * output[s_node]
+                
+        below is the implementation of delta[t_node] = (dEtotal/dout[t_node]) x (dout[t_node]/dnet[t_node])
+        """
+        #if at output layer, do delta rule 
+        if layer==self.number_of_layer:
+            return -(float(target[t_node])-output[layer].item(t_node)) * output[layer].item(t_node) * (1-output[layer].item(t_node))
+        
+        #else, do recursive until output layer 
+        else:        
+            total = 0
+            for i in range(len(output[layer+1])):
+                    total += self.delta(layer+1,i,t_node,output,target)*self.weights[layer].item((i,t_node))
+
+            total = total*output[layer].item(t_node)*(1-output[layer].item(t_node))
+
+            return total
+
+    def update_delta_weights(self,output,target):
+        """
+        save each weight update value to delta_weight, when need to update to weight call func update_weight
+        delta_weight will be set to 0
+        """
+        for i in range(0,self.number_of_layer):
+            for j in range(len(self.delta_weights[i])):
+                for k in range(len(self.delta_weights[i][j])):
+                    self.delta_weights[i][j][k] += self.delta(i+1,j,k,output,target)*output[i].item(k)
+                self.delta_biases[i][j] += self.delta(i+1,j,-1,output,target)
+    
+    def update_weights(self):
+        """
+        weight being updated by the value of delta_weight
+        """
+        for i in range(self.number_of_layer):
+            for j in range(len(self.weights[i])):
+                for k in range(len(self.delta_weights[i][j])):
+                    self.weights[i][j][k] -= self.learning_rate*self.delta_weights[i][j][k]
+                    self.delta_weights[i][j][k] = 0
+                self.biases[i][j] -= self.learning_rate*self.delta_biases[i][j]
+                self.delta_biases[i][j] = 0
         
 
 
 if __name__ == "__main__":
-    model = mlp(2)
+    model = mlp(2,.25,2)
     model.add_layer(2)
     model.add_layer(2)
 
-    model.train(training_input,training_output)
+    model.train(training_input,training_output,50)
     print(training_xor[0]['input'])
     output = model.feed_forward(training_xor[0]['input'])
     print(output[-1])
